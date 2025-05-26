966ccabc93ef5d43a6bf670f3db2a4497b335f6e
Jimmy <jimmy@pic-digital.com>
2025-05-26 13:03:01 +0200

    target page

--- components/targeted-locations/TargetedLocations.tsx
+++ components/targeted-locations/TargetedLocations.tsx
@@ -14,53 +14,56 @@ function TargetedLocations({targetedLocations}: {targetedLocations: Post[]}) {
 
   return (
     <div className="mb-40">
-      <h1 className="flex flex-col text-[128px] font-fontBold">
+      <h1 className="flex flex-col text-5xl sm:text-7xl lg:text-[128px] font-fontBold">
         <span>LES</span>
         <span>ESPACES</span>
         <span>CIBLÉS</span>
       </h1>
 
-      <p className="bg-primary text-white text-4xl min-w-1/2 max-w-[650px] mt-0 mb-1 p-1 font-bold">
+      <p className="bg-primary text-white text-lg sm:text-2xl lg:text-4xl w-fit max-w-[650px] mt-0 mb-1 p-1 font-bold">
         ARTICHEKT se donne pour mission
       </p>
-      <p className="bg-primary text-white text-4xl min-w-1/2 max-w-[650px] my-0 p-1 mb-5 font-bold">
+      <p className="bg-primary text-white text-lg sm:text-2xl lg:text-4xl w-fit max-w-[650px] my-0 p-1 mb-40 lg:mb-5 font-bold">
         la création d’espaces pluriels.
       </p>
 
       {targetedLocations.length > 0 &&
         targetedLocations.map(
           ({featuredImage, content, title, tags, databaseId}, index) => (
-            <article key={databaseId} className="mb-10">
+            <article
+              key={databaseId}
+              className="mb-10 flex flex-col-reverse lg:flex-col"
+            >
               <div
-                className={`flex gap-8 items-center px-8 ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}`}
+                className={`flex flex-col gap-8 items-center sm:px-8 ${index % 2 === 0 ? 'lg:flex-row' : 'lg:flex-row-reverse'}`}
               >
-                <div className="w-1/2">
+                <div className="w-full lg:w-1/2">
                   <Image
                     width={600}
                     height={400}
-                    className="h-[600px] object-cover filter grayscale"
+                    className="sm:h-[600px] w-full object-cover filter grayscale"
                     alt={featuredImage.node.altText}
                     src={featuredImage.node.sourceUrl}
                   />
                 </div>
 
                 <div
-                  className="w-1/2 text-2xl"
+                  className="lg:w-1/2 text-lg sm:text-2xl"
                   dangerouslySetInnerHTML={{__html: content}}
                 />
               </div>
               <div
-                className={`flex gap-8  ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}`}
+                className={`flex flex-col gap-10  ${index % 2 === 0 ? 'lg:flex-row' : 'lg:flex-row-reverse'}`}
               >
-                <div className="w-1/2 pr-8">
+                <div className="lg:w-1/2">
                   <h3
-                    className={`text-7xl m-0  ${index % 2 === 0 ? 'text-end' : 'text-start'}`}
+                    className={`text-4xl sm:text-5xl text-center lg:text-6xl m-0  ${index % 2 === 0 ? 'lg:text-end' : 'lg:text-start'}`}
                   >
                     {title}
                   </h3>
                 </div>
                 <div
-                  className={`w-1/2 flex items-end ${index % 2 === 0 ? 'justify-start' : 'justify-end'}`}
+                  className={`lg:w-1/2 flex items-end justify-center ${index % 2 === 0 ? 'lg:justify-start' : 'lg:justify-end'}`}
                   style={{gap: tags.nodes.length < 3 ? '6vw' : '3vw'}}
                 >
                   {tags &&
@@ -75,18 +78,18 @@ function TargetedLocations({targetedLocations}: {targetedLocations: Post[]}) {
                           height={87}
                           src={tag.tagIcon}
                           alt={tag.name}
-                          className="m-0 mb-5 w-20"
+                          className="m-0 mb-5 w-10 sm:w-20"
                         />
                         <div>
                           {!displayTagName(tag.name).length ? (
-                            <p className="text-center bg-primary font-fontBold text-white px-1 text-xl m-1 mb-8">
+                            <p className="text-center bg-primary font-fontBold text-white px-1 text-xs sm:text-xl m-1 mb-8">
                               {tag.name}
                             </p>
                           ) : (
                             displayTagName(tag.name).map((part, index) => (
                               <p
                                 key={index}
-                                className="text-center bg-primary font-fontBold text-white px-1 text-xl m-1"
+                                className="text-center bg-primary font-fontBold text-white px-1 text-xs sm=text-xl m-1"
                               >
                                 {part}
                               </p>
